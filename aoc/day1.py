307e35fbd07d6fdf024d1fccb002cb9939c10314
Dorian Van den Heede <devdnhee@gmail.com>
2022-12-03 14:56:18 +0100

    day 1

---
+++ aoc/day1.py
@@ -0,0 +1,23 @@
+
+def get_calories(elf):
+    food = elf.split('\n')
+    return sum(map(int, food))
+
+
+def get_max_calories(fp):
+    inp = open(fp).read().split('\n\n')
+    return max(map(get_calories, inp))
+
+def get_sum_top_3_calories(fp):
+    inp = open(fp).read().split('\n\n')
+    return sum(list(reversed(sorted(map(get_calories, inp))))[:3])
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # first star
+    print(get_max_calories('../tests/1.txt'))
+    print(get_max_calories('../input/1.txt'))
+
+    # second star
+    print(get_sum_top_3_calories('../tests/1.txt'))
+    print(get_sum_top_3_calories('../input/1.txt'))
