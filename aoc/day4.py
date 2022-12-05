bfc4a7ee2dbd2d073b5beb42e7b3772e82405bbc
Dorian Van den Heede <devdnhee@gmail.com>
2022-12-05 13:17:55 +0100

    day 4 + 5

---
+++ aoc/day4.py
@@ -0,0 +1,35 @@
+import re
+
+RE_SECTIONS = re.compile("^(\d+)-(\d+),(\d+)-(\d+)\s*$")
+
+def is_overlap(line, full=True):
+    """
+
+    :param line: eg. 2-4,6-8
+    :return:
+    """
+    s1_min, s1_max, s2_min, s2_max = [int(g) for g in RE_SECTIONS.match(line).groups()]
+    if full:
+        return (s1_min <= s2_min and s1_max >= s2_max) or (s2_min <= s1_min and s2_max >= s1_max)
+    else:
+        return not (s1_max < s2_min or s2_max < s1_min)
+
+
+def get_total_number_of_overlaps(fp, full=True):
+    inp = open(fp).readlines()
+    return sum(
+        map(
+            lambda x: is_overlap(x, full), inp
+        )
+    )
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # First star
+    print(get_total_number_of_overlaps('../tests/4.txt'))
+    print(get_total_number_of_overlaps('../input/4.txt'))
+
+    # Second star
+    print(get_total_number_of_overlaps('../tests/4.txt', full=False))
+    print(get_total_number_of_overlaps('../input/4.txt', full=False))
