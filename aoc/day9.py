5b867ce3c787c5a610d062f5b72ce7f61fcefc06
Dorian Van den Heede <devdnhee@gmail.com>
2022-12-09 15:14:02 +0100

    black day 9

--- aoc/day9.py
+++ aoc/day9.py
@@ -1,14 +1,15 @@
 import math
 import re
 
-RE_COMMAND = re.compile(r'^([RLUD])\s(\d+)\s*$')
+RE_COMMAND = re.compile(r"^([RLUD])\s(\d+)\s*$")
 
-class Rope():
+
+class Rope:
     DIRECTIONS = {
-        'R': complex(1, 0),
-        'L': complex(-1, 0),
-        'U': complex(0, 1),
-        'D': complex(0, -1)
+        "R": complex(1, 0),
+        "L": complex(-1, 0),
+        "U": complex(0, 1),
+        "D": complex(0, -1),
     }
 
     def __init__(self, head, n_knots=2):
@@ -28,27 +29,25 @@ class Rope():
     def diagonal_direction(self, delta):
         return complex(abs(delta.real) / delta.real, abs(delta.imag) / delta.imag)
 
-
     def make_move(self, direction):
         # make move on head
         self.rope[0] += Rope.DIRECTIONS[direction]
 
         for i in range(1, self.length):
-            if self.is_adjacent(self.rope[i-1], self.rope[i]):
+            if self.is_adjacent(self.rope[i - 1], self.rope[i]):
                 continue
 
             # move one step in direction to previous node if gap
-            if self.check_gap(self.rope[i], self.rope[i-1]):
-                delta = (self.rope[i - 1] - self.rope[i])
-                self.rope[i] += (delta / 2)
+            if self.check_gap(self.rope[i], self.rope[i - 1]):
+                delta = self.rope[i - 1] - self.rope[i]
+                self.rope[i] += delta / 2
                 continue
 
             # diagonal movement
-            delta = (self.rope[i-1] - self.rope[i])
+            delta = self.rope[i - 1] - self.rope[i]
             self.rope[i] += self.diagonal_direction(delta)
 
         self.state_history.append(tuple(self.rope))
-
 
     def run_command(self, command):
         direction, steps = RE_COMMAND.match(command).groups()
@@ -56,22 +55,24 @@ class Rope():
         for step in range(steps):
             self.make_move(direction)
 
-
     def nbr_different_tail_states(self):
         return len(set(map(lambda t: t[-1], self.state_history)))
 
-
     def __str__(self):
-        min_x, max_x = min(int(min([r.real for r in self.rope])), 0), max(int(max([r.real for r in self.rope])), 0)
-        min_y, max_y = min(int(min([r.imag for r in self.rope])), 0), max(int(max([r.imag for r in self.rope])), 0)
-        grid = [(max_x - min_x + 1) * ['.'] for i in range(max_y - min_y + 1)]
-        grid[len(grid)+min_y-1][-min_x] = "s"
+        min_x, max_x = min(int(min([r.real for r in self.rope])), 0), max(
+            int(max([r.real for r in self.rope])), 0
+        )
+        min_y, max_y = min(int(min([r.imag for r in self.rope])), 0), max(
+            int(max([r.imag for r in self.rope])), 0
+        )
+        grid = [(max_x - min_x + 1) * ["."] for i in range(max_y - min_y + 1)]
+        grid[len(grid) + min_y - 1][-min_x] = "s"
         for idx, node in enumerate(self.rope):
             r = len(grid) - (int(node.imag) - min_y) - 1
             c = int(node.real) - min_x
-            string = 'H' if idx == 0 else idx
-            grid[r][c] = f"{string}" if grid[r][c] == '.' else f"{grid[r][c]}{string}"
-        s = '\n'.join(['\t'.join(row) for row in grid])
+            string = "H" if idx == 0 else idx
+            grid[r][c] = f"{string}" if grid[r][c] == "." else f"{grid[r][c]}{string}"
+        s = "\n".join(["\t".join(row) for row in grid])
         return s
 
 
