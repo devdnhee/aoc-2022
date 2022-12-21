f7b7aea30ac9534ec4afe68dd7ea157e9480c49d
Dorian Van den Heede <devdnhee@gmail.com>
2022-12-21 14:29:18 +0100

    :elephant: day 17 part 2

--- aoc/day17.py
+++ aoc/day17.py
@@ -1,6 +1,7 @@
 from itertools import cycle, islice
 import numpy as np
 from tqdm import tqdm
+import pandas as pd
 
 class Chamber:
     
@@ -30,26 +31,36 @@ class Chamber:
         UNDERSCORE, PLUS, JAY, LINE, SQUARE
     ]
     
+    TAIL_CHECK = 400
+    
     @classmethod
     def move_rock(cls, rock, offset):
         return [r + offset for r in rock]
     
     def __init__(self, width=7) -> None: 
         self._width = width
+        self._height = 0
         self.environment = [
             complex(i, 0) for i in range(width)
         ]
         self._falling_rock = None
+        self.history = []
 
 
     @property
     def height(self):
-        return int(max(map(lambda pos: pos.imag, self.environment)))
+        return int(self._height)
+    
+    
+    @height.setter
+    def height(self, value):
+        self._height = value
+        return self._height
     
     
     @property
     def floor(self):
-        return int(min(map(lambda pos: pos.imag, self.environment)))
+        return int(min(map(lambda pos: pos.imag, self.environment[-Chamber.TAIL_CHECK:])))
     
     
     @property
@@ -99,6 +110,14 @@ class Chamber:
                 matrix[r, c] = -1
         
         return matrix
+    
+    
+    @property
+    def df_simulation(self):
+        df = (pd.DataFrame(self.history, columns=['number_moves', 'n_rocks', 'height'])
+              .assign(height_rock_ratio = lambda df: df.height / df.n_rocks, 
+                     moves_for_rock = lambda df: df['number_moves'].diff()))
+        return df
         
         
     def rock_to_matrix(self, rock):
@@ -113,11 +132,9 @@ class Chamber:
     
     def check_move(self, move):
         moved_rock = Chamber.move_rock(self.falling_rock, move)
-        # rock_matrix = self.rock_to_matrix(moved_rock)
-        # return np.sum(np.logical_and(self.matrix > 0, rock_matrix > 0)) == 0
         if moved_rock[-1].real >= self.width or moved_rock[0].real < 0:
             return False
-        if any(map(lambda pos: pos in self.environment, moved_rock)):
+        if any(map(lambda pos: pos in self.environment[-Chamber.TAIL_CHECK:], moved_rock)):
             return False
         return True
     
@@ -130,16 +147,63 @@ class Chamber:
     def update_environment(self):
         self.environment.extend(self.falling_rock)
         self.falling_rock = None
+        
+        
+    def find_pattern_period(self, pattern_length):
+        df = self.df_simulation
+        if len(df) < 2 * pattern_length:
+            # Impossible to detect a pattern
+            return None
+        
+        # try to backtrack the last pattern we've seen
+        pattern = df['moves_for_rock'].tail(pattern_length).values
+        
+        # take equal segments
+        sequence = df['moves_for_rock'].values[len(df) % pattern_length:]
+        split_sequence = np.hsplit(sequence, len(sequence) / pattern_length)
+        
+        # check where the pattern reoccurred
+        matches = np.atleast_1d(np.squeeze(np.argwhere(list(map(lambda a: np.all(pattern == a), split_sequence)))))
+        block_index = np.unique(np.diff(matches))
+        if len(block_index) == 0:
+            return None
+        elif len(block_index) == 1:
+            return block_index[0] * pattern_length
+        else:
+            # Multiple patterns discovered!
+            raise Exception(f'Multiple patterns found for min_f={pattern_length}')
                 
                 
-    def simulation(self, moves, N=2022):
+    def simulation(self, moves, N=2022, find_repeats=False, pattern_length=2000):
         rock_generator = islice(self.generate_rocks(), N)
         moves_generator = cycle(moves)
-        for move_enc in moves_generator:
+        pattern_divisor = len(moves) * 5
+        pattern_modulo = N % pattern_divisor
+        
+        print(f"Start simulation: N={N}, M={len(moves)}, min_freq={pattern_length}")
+        n_rocks=0
+        cycle_period = None
+        for m_idx, move_enc in enumerate(moves_generator):
             move = Chamber.MOVES[move_enc]
             if self.falling_rock is None:
+                self.history.append((m_idx, n_rocks, self.height))
+                if find_repeats and cycle_period is None and n_rocks > 1 and n_rocks % pattern_divisor == pattern_modulo:
+                    print(f"Checking for pattern after {n_rocks} rocks: pattern_length={pattern_length}")
+                    cycle_period = self.find_pattern_period(pattern_length)
+                    self.df_simulation.to_csv('df_simulation.csv')
+                    if cycle_period is not None:
+                        print(f"Detected pattern after {n_rocks} rocks: cycle_period={cycle_period}")
+                
+                if cycle_period is not None and (n_rocks % cycle_period == N % cycle_period):
+                    height_per_cycle = self.height - self.df_simulation.iloc[-cycle_period-1].height
+                    remaining_cycles = (N - n_rocks) / cycle_period
+                    height = self.height + remaining_cycles * height_per_cycle
+                    print(f"Calculating height after {n_rocks} rocks: height={height}")
+                    return height
+                    
                 try:
                     self.falling_rock = next(rock_generator)
+                    n_rocks += 1
                 except StopIteration:
                     return self.height
 
@@ -150,6 +214,8 @@ class Chamber:
                 self.falling_rock = Chamber.move_rock(self.falling_rock, Chamber.MOVES['v'])
             else:
                 self.environment.extend(self.falling_rock)
+                self.environment = self.environment[-2*Chamber.TAIL_CHECK:]
+                self.height = int(max(map(lambda pos: pos.imag, self.environment[-Chamber.TAIL_CHECK:])))
                 self.falling_rock = None
                 
         
@@ -160,15 +226,16 @@ class Chamber:
         return drawing
 
 
-def simulation(fp, N):
+def simulation(fp, **kwargs):
     move_list = open(fp).read().rstrip()
     
     chamber = Chamber()
-    chamber.simulation(move_list, N)
-    
-    return chamber.height
+    return chamber.simulation(move_list, **kwargs)
 
 
 if __name__ == '__main__':
-    print(simulation('tests/17.txt', N=2022))
-    print(simulation('input/17.txt', N=2022))
+    # print(simulation('tests/17.txt', N=2022))
+    # print(simulation('input/17.txt', N=2022))
+    print(simulation('tests/17.txt', N=5000, find_repeats=True))
+    print(simulation('tests/17.txt', N=1000000000000, find_repeats=True))
+    print(simulation('input/17.txt', N=1000000000000, find_repeats=True))
